29597c0f8f1ff120f5762c9a7d93078a4d10925e
daianalop <daiana.a.lopez@gmail.com>
2020-09-05 15:04:32 -0300

    Primer commit tp1

---
+++ TP1_CALCULADORA/src/TP1_CALCULADORA.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : TP1_CALCULADORA.c
+ Author      : Daiana Andrea Lopez
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+
+
+	return EXIT_SUCCESS;
+}
